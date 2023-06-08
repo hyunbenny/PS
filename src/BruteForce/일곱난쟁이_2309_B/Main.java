cbfc1b2ac7067102f36c0f1363e75fecd7e2e40f
hbcho <bluesky3268@gmail.com>
2023-06-08 14:09:30 +0900

    완전탐색 실버 : 2309_일곱난쟁이

---
+++ src/BruteForce/일곱난쟁이_2309_B/Main.java
@@ -0,0 +1,48 @@
+package BruteForce.일곱난쟁이_2309_B;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] total = new int[9];
+        int sum = 0;
+
+        int fake1 = 0;
+        int fake2 = 0;
+
+        // 9명의 키를 다 더한다.
+        for (int i = 0; i < total.length; i++) {
+            total[i] = Integer.parseInt(br.readLine());
+            sum += total[i];
+        }
+
+        Arrays.sort(total);
+
+        // 9명 중 2명을 제외한 합이 100이 되는지 확인
+        for (int i = 0; i < total.length; i++) {
+            for (int j = i + 1; j < total.length; j++) {
+                if (isSameAs100(sum, total[i], total[j])) {
+                    fake1 = i;
+                    fake2 = j;
+                    break;
+                }
+            }
+        }
+
+        // 가짜 2개의 값을 제외한 나머지를 출력한다.
+        for (int i = 0; i < total.length; i++) {
+            if(i == fake1 || i == fake2) continue;
+            System.out.println(total[i]);
+        }
+
+    }
+
+    private static boolean isSameAs100(int sum, int i, int j) {
+        return (sum - i - j) == 100;
+    }
+}
