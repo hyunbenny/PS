b5ad383f3f0f57fd9a37399d1e37e2e6cb95d388
hbcho <bluesky3268@gmail.com>
2023-06-08 14:13:48 +0900

    완전탐색 브론즈 : 2309_일곱난쟁이

--- src/BruteForce/일곱난쟁이_2309_B/Main.java
+++ src/BruteForce/일곱난쟁이_2309_B/Main.java
@@ -6,6 +6,9 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
+
+    // https://www.acmicpc.net/problem/2309
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
